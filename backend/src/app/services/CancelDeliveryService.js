87190e8d699bbf01ff4f6cdb388193f6403d3c7d
Dtesch9 <alucard.s8123@gmail.com>
2020-04-07 13:12:08 -0300

    Services: CancelDelivery

---
+++ backend/src/app/services/CancelDeliveryService.js
@@ -0,0 +1,61 @@
+import DeliveryProlem from '../models/DeliveryProblem';
+import Package from '../models/Package';
+import Deliveryman from '../models/Deliveryman';
+import Recipient from '../models/Recipient';
+
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
+
+class CancelDeliveryService {
+  async run({ problem_id }) {
+    const deliveryPackage = await DeliveryProlem.findOne({
+      where: { id: problem_id },
+      include: [
+        {
+          model: Package,
+          as: 'delivery',
+          attributes: { exclude: ['createdAt', 'updatedAt'] },
+          include: [
+            {
+              model: Deliveryman,
+              as: 'delivery_man',
+            },
+            {
+              model: Recipient,
+              as: 'recipient',
+            },
+          ],
+        },
+      ],
+    });
+
+    if (!deliveryPackage) {
+      throw new Error('Package does not exists');
+    }
+
+    /**
+     * Check if package has already been cancelled
+     */
+    if (deliveryPackage.delivery.canceled_at) {
+      throw new Error('Package has already been cancelled');
+    }
+
+    await Package.update(
+      { canceled_at: new Date() },
+      { where: { id: deliveryPackage.delivery.id } }
+    );
+
+    /**
+     * Cancelation e-mail
+     */
+    await Queue.add(CancellationMail.key, {
+      deliveryPackage,
+    });
+
+    /**
+     * Invalidate cache
+     */
+  }
+}
+
+export default new CancelDeliveryService();
