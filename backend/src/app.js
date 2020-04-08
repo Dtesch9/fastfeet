8cbd424d1c398a3bfac0754e4c7c98cd17b86a45
Dtesch9 <alucard.s8123@gmail.com>
2020-04-08 10:05:51 -0300

    Request limit with RateLimit

--- backend/src/app.js
+++ backend/src/app.js
@@ -42,18 +42,20 @@ class App {
       express.static(resolve(__dirname, '..', 'tmp', 'uploads'))
     );
 
-    this.server.use(
-      new RateLimit({
-        store: new RateLimitRedis({
-          client: redis.createClient({
-            host: process.env.REDIS_HOST,
-            port: process.env.REDIS_PORT,
+    if (process.env.NODE_ENV === 'development') {
+      this.server.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
           }),
-        }),
-        windowMs: 1000 * 60 * 15,
-        max: 100,
-      })
-    );
+          windowMs: 1000 * 60 * 15,
+          max: 100,
+        })
+      );
+    }
   }
 
   routes() {
